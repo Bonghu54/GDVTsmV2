f7585a5ecf397ae375220c5d9a373ad4376a803c
CesarLop3z <g_dragon_game@hotmail.com>
2023-11-04 04:22:24 -0400

    Modelos_Cesar

---
+++ Models/Detalle_Producto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GDVTsmV3.Models
+{
+    public class Detalle_Producto
+    {
+        [Key]
+        public int Id_Detalle_Producto { get; set; }
+        public string Color { get; set; }
+        public string Tipo_De_Uso { get; set; }
+        public string Durabilidad { get; set; }
+        public string Material { get; set; }
+        public string Estampado { get; set; }
+
+        [ForeignKey("Producto")]
+        public int Id_Producto { get; set; }
+        public Producto Producto { get; set; }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GDVTsmV3.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id_Producto { get; set; }
+        public string Nombre_Producto { get; set; }
+        public string Descripcion { get; set; }
+        public string Unidad_De_Venta { get; set; }
+        public string Tipo_Producto { get; set; }
+        public decimal Precio_Unitario { get; set; }
+        public int Cantidad_En_Stock { get; set; }
+        public ICollection<Detalle_Producto> Detalles_Producto { get; set; }
+
+    }
+}
